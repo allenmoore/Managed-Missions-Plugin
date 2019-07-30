51e833fe3c45ac8c468d98a3466f39da316d996a
Allen Moore <am@allenmoore.co>
2019-07-29 23:25:56 -0400

    update node dependencies

--- gulp-tasks/postcss.js
+++ gulp-tasks/postcss.js
@@ -1,17 +1,12 @@
 import atImport from 'postcss-import';
 import chalk from 'chalk';
-import cssnano from 'gulp-cssnano';
 import gulp from 'gulp';
-import inlineSVG from 'postcss-inline-svg';
-import livereload from 'gulp-livereload';
-import mixins from 'postcss-mixins';
+import pixrem from 'pixrem';
 import postcss from 'gulp-postcss';
 import presetEnv from 'postcss-preset-env';
 import rename from 'gulp-rename';
 import rgbaFallback from 'postcss-color-rgba-fallback';
 import sourcemaps from 'gulp-sourcemaps';
-import svg from 'postcss-svg';
-import vars from 'postcss-simple-vars';
 
 const log = console.log;
 
@@ -32,22 +27,26 @@ gulp.task('postcss', () => {
   };
   log(chalk.redBright('--- Running PostCSS Goodness ---'));
 
-  return gulp.src(opts.src)
-    .pipe(sourcemaps.init({loadMaps: true}))
-	  .pipe(postcss([
-      atImport(),
-      mixins(),
-      vars(),
-      presetEnv({
-        stage: 0
-      }),
-      inlineSVG({
-        path: './dist/svg'
-      }),
-      rgbaFallback()
-    ]))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(opts.dest))
-    .pipe(livereload());
+	return gulp.src(opts.src)
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(postcss([
+			atImport(),
+			presetEnv({
+				stage: 2,
+				features: {
+					'custom-media-queries': true,
+					'custom-properties': true,
+					'image-set-function': true,
+					'matches-pseudo-class': true,
+					'media-query-ranges': true,
+					'nesting-rules': true,
+					'not-pseudo-class': true
+				}
+				}),
+			pixrem(),
+			rgbaFallback()
+		]))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(opts.dest));
 });
 
